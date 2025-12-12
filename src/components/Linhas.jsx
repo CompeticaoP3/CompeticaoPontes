36ad389a1d9aed482b6ff9a5f0576075a72bd516
carlosabrantesdev <carlosduartee128@gmail.com>
2025-12-12 08:49:47 -0300

    Fix v1.1.4

--- src/components/Linhas.jsx
+++ src/components/Linhas.jsx
@@ -1,46 +1,49 @@
-import { useState } from 'react'
 import '../Pontes.css'
 
 function Linhas(props) {
-    const propsStyle = {
-        width: props.kilo === '5KG' ? "13vh" : "18vh",
-        backgroundColor: props.tipo === 'Carga Atual' ? '#507d46' : '#006494',
-        display: props.visivel === true ? '' : 'none'
-    };
+  const deveMostrar =
+    props.visivel === true &&
+    !(props.kilo === '5KG' && !props.primeiroClique);
 
-    return (
-        <div className='linhas'>
-            <div className='linha'>
-                <div className='esquerda-linha'>
-                    {props.tipo && (
-                        <div className="caixa" id="tipo">
-                            <p>{props.tipo}</p>
-                        </div>
-                    )}
-                </div>
+  const propsStyle = {
+    width: props.kilo === '5KG' ? "13vh" : "18vh",
+    backgroundColor: props.tipo === 'Carga Atual' ? '#507d46' : '#006494',
+    display: deveMostrar ? '' : 'none'
+  };
 
-                <div className='meio-linha'>
-                    {props.kilo && (
-                        <div
-                            className="caixa"
-                            id="kilo"
-                            style={propsStyle}
-                        >
-                            <p>{props.kilo}</p>
-                        </div>
-                    )}
-                </div>
-
-                <div className='direita-linha'>
-                    {props.kilorecorde && (
-                        <div className="caixa" id="kilorecorde">
-                            <p>{props.kilorecorde}</p>
-                        </div>
-                    )}
-                </div>
+  return (
+    <div className='linhas'>
+      <div className='linha'>
+        <div className='esquerda-linha'>
+          {props.tipo && (
+            <div className="caixa" id="tipo">
+              <p>{props.tipo}</p>
             </div>
+          )}
         </div>
-    )
+
+        <div className='meio-linha'>
+          {props.kilo && (
+            <div
+              className="caixa"
+              id="kilo"
+              style={propsStyle}
+            >
+              <p>{props.kilo}</p>
+            </div>
+          )}
+        </div>
+
+        <div className='direita-linha'>
+          {props.kilorecorde && (
+            <div className="caixa" id="kilorecorde">
+              <p>{props.kilorecorde}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Linhas
+export default Linhas;
